56d49509d026df5cce95261a2c37ee4b916da7e2
Daniel Quintero <dgquintero02@hotmail.com>
2020-04-22 16:05:48 -0500

    adding states_list files

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Script that starts a Flask web application
+'''
+from models import storage
+from models import *
+from flask import Flask, escape, url_for, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states_list():
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_storage():
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
